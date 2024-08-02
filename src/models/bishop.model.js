d4dd054ba04fbc11c4cceeb222aaf05e2bee2718
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-08-02 14:02:30 +0100

    Update bishop.model.js

--- src/models/bishop.model.js
+++ src/models/bishop.model.js
@@ -1,3 +1,4 @@
+
 import Piece from './piece.model.js';
 
 export default class Bishop extends Piece {
@@ -7,7 +8,32 @@ export default class Bishop extends Piece {
 
   generatePossibleMoves(board) {
     const moves = [];
-    // Implement bishop-specific move logic here
+    const [file, rank] = this.position.split('');
+    const currentRank = parseInt(rank);
+    const fileIndex = file.charCodeAt(0);
+
+    const directions = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+
+    directions.forEach(([fDir, rDir]) => {
+      for (let i = 1; i < 8; i++) {
+        const newFile = fileIndex + i * fDir;
+        const newRank = currentRank + i * rDir;
+        if (newFile >= 97 && newFile <= 104 && newRank >= 1 && newRank <= 8) {
+          const move = `${String.fromCharCode(newFile)}${newRank}`;
+          if (!board[move]) {
+            moves.push(move);
+          } else {
+            if (board[move].color !== this.color) {
+              moves.push(move);
+            }
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    });
+
     return moves;
   }
 }
