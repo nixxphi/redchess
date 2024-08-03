0873cdf91c06b29f8f2bb586105ae2ee432a10cd
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-08-03 20:42:05 +0100

    Update queen.model.js

--- src/models/queen.model.js
+++ src/models/queen.model.js
@@ -1,3 +1,4 @@
+// ./models/queen.model.js
 
 import Piece from './piece.model.js';
 
@@ -8,7 +9,35 @@ export default class Queen extends Piece {
 
   generatePossibleMoves(board) {
     const moves = [];
-    // Implement queen-specific move logic here
+    const [file, rank] = this.position.split('');
+    const currentRank = parseInt(rank);
+    const fileIndex = file.charCodeAt(0);
+
+    const directions = [
+      [1, 1], [1, -1], [-1, 1], [-1, -1], // Diagonal
+      [1, 0], [-1, 0], [0, 1], [0, -1]   // Straight
+    ];
+
+    directions.forEach(([fDir, rDir]) => {
+      for (let i = 1; i < 8; i++) {
+        const newFile = fileIndex + i * fDir;
+        const newRank = currentRank + i * rDir;
+        if (newFile >= 97 && newFile <= 104 && newRank >= 1 && newRank <= 8) {
+          const move = `${String.fromCharCode(newFile)}${newRank}`;
+          if (!board[move]) {
+            moves.push(move);
+          } else {
+            if (board[move].color !== this.color) {
+              moves.push(move);
+            }
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    });
+
     return moves;
   }
 }
