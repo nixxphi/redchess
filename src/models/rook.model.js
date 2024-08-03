8fa8681542c0e417aa0ab3f1377deba46aa5878d
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-08-03 20:38:49 +0100

    Update rook.model.js

--- src/models/rook.model.js
+++ src/models/rook.model.js
@@ -7,7 +7,32 @@ export default class Rook extends Piece {
 
   generatePossibleMoves(board) {
     const moves = [];
-    // Implement rook-specific move logic here
+    const [file, rank] = this.position.split('');
+    const currentRank = parseInt(rank);
+    const fileIndex = file.charCodeAt(0);
+
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+    directions.forEach(([fDir, rDir]) => {
+      for (let i = 1; i < 8; i++) {
+        const newFile = fileIndex + i * fDir;
+        const newRank = currentRank + i * rDir;
+        if (newFile >= 97 && newFile <= 104 && newRank >= 1 && newRank <= 8) {
+          const move = `${String.fromCharCode(newFile)}${newRank}`;
+          if (!board[move]) {
+            moves.push(move);
+          } else {
+            if (board[move].color !== this.color) {
+              moves.push(move);
+            }
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    });
+
     return moves;
   }
 }
