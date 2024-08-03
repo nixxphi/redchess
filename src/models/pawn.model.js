15cb15f07dcc8093627021ed0ba1a9f7882a3910
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-08-03 20:35:01 +0100

    Update pawn.model.js

--- src/models/pawn.model.js
+++ src/models/pawn.model.js
@@ -1,3 +1,5 @@
+// ./models/pawn.model.js
+
 import Piece from './piece.model.js';
 
 export default class Pawn extends Piece {
@@ -7,7 +9,33 @@ export default class Pawn extends Piece {
 
   generatePossibleMoves(board) {
     const moves = [];
-    // Implement pawn-specific move logic here
+    const direction = this.color === 'white' ? 1 : -1; // White pawns move up, black pawns move down
+    const [file, rank] = this.position.split('');
+    const currentRank = parseInt(rank);
+
+    // Standard move
+    const forwardMove = `${file}${currentRank + direction}`;
+    if (!board[forwardMove]) {
+      moves.push(forwardMove);
+      // Initial double move
+      if (!this.hasMoved) {
+        const doubleMove = `${file}${currentRank + 2 * direction}`;
+        if (!board[doubleMove]) {
+          moves.push(doubleMove);
+        }
+      }
+    }
+
+    // Captures
+    const captureLeft = `${String.fromCharCode(file.charCodeAt(0) - 1)}${currentRank + direction}`;
+    const captureRight = `${String.fromCharCode(file.charCodeAt(0) + 1)}${currentRank + direction}`;
+    if (board[captureLeft] && board[captureLeft].color !== this.color) {
+      moves.push(captureLeft);
+    }
+    if (board[captureRight] && board[captureRight].color !== this.color) {
+      moves.push(captureRight);
+    }
+
     return moves;
   }
 }
